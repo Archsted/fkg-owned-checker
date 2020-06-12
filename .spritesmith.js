7b0e966b6bc40ee125d08ba6fc34cf9212473e48
Archsted <archsted@gmail.com>
2020-06-13 06:15:47 +0900

    キャラクターのスプライト画像を分割

--- .spritesmith.js
+++ .spritesmith.js
@@ -2,13 +2,23 @@
 
 var util = require('util');
 
-module.exports = {
-    src: './src/assets/images/character-icon/*.png',
-//    destImage: './public/images/character-icon.png',
-    destImage: './src/assets/character-icon.png',
-    destCSS: './src/assets/character-icon.css',
-    imgPath: './character-icon.png',
-    padding: 0,
-    algorithm: 'binary-tree',
-    cssTemplate: 'css-sprite.template',
-};
+module.exports = [
+    {
+        src: './src/assets/images/character-icon/gacha6/*.png',
+        destImage: './src/assets/character-icon-gacha6.png',
+        destCSS: './src/assets/character-icon-gacha6.css',
+        imgPath: './character-icon-gacha6.png',
+        padding: 0,
+        algorithm: 'binary-tree',
+        cssTemplate: 'css-sprite.template',
+    },
+    {
+        src: './src/assets/images/character-icon/gacha5/*.png',
+        destImage: './src/assets/character-icon-gacha5.png',
+        destCSS: './src/assets/character-icon-gacha5.css',
+        imgPath: './character-icon-gacha5.png',
+        padding: 0,
+        algorithm: 'binary-tree',
+        cssTemplate: 'css-sprite.template',
+    },
+];
